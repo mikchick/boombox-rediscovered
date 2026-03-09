2297626c90a10bfb3872962df99b3d81ba87be9a
mikchick <mikifujiwara.art@gmail.com>
2026-03-08 18:08:57 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   base: "/boombox-rediscovered/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
-
